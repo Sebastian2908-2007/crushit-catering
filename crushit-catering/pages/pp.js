b4cd15258d807f4500457ee07a0ff8f6ddec89d5
Sebabin <sebastiancrespin279@gmail.com>
2023-02-02 14:03:52 -0700

    added a tos page as well as privacy policy page added said links in footer along with social links and icons all looks well at multiple sizes I'm leaving this branch to the history books all work was done in the layout component

---
+++ crushit-catering/pages/pp.js
@@ -0,0 +1,34 @@
+const Pp = () => {
+    return(
+        <p>
+Introduction
+At [Restaurant Name], we are committed to protecting the privacy of our users. This Privacy Policy outlines the information we collect, how we use it, and the choices you have with respect to your information.
+
+Information We Collect
+Account Information: When you create an account on our app using your Google, Twitter, or email account, we may collect your name, email address, and profile picture.
+Usage Information: We may collect information about your use of our app, such as the pages you visit and the actions you take within our app.
+Location Information: We may collect information about your location to provide you with location-based services, such as restaurant recommendations.
+Use of Information
+To provide and improve our app: We use the information we collect to provide and improve our app and its features.
+To send you marketing communications: We may use your email address to send you marketing communications about our restaurant and its services. You may opt-out of these communications at any time.
+To comply with legal obligations: We may use your information as necessary to comply with our legal obligations, such as responding to subpoenas or court orders.
+Sharing of Information
+Service Providers: We may share your information with service providers who assist us in operating our app, such as hosting and marketing services.
+Legal Requirements: We may be required to disclose your information in response to legal process, such as a subpoena or court order.
+Security
+We take reasonable measures to protect your information from unauthorized access and disclosure. However, no security measures are foolproof and we cannot guarantee the security of your information.
+
+Data Retention
+We will retain your information for as long as necessary to provide you with the services you have requested.
+
+Changes to Privacy Policy
+We may update this Privacy Policy from time to time to reflect changes in our practices or to comply with legal requirements. Your continued use of our app after any changes indicates your acceptance of the revised Privacy Policy.
+
+Contact Us
+If you have any questions or concerns regarding this Privacy Policy, please contact us at [Contact Information].
+
+        </p>
+    );
+};
+
+export default Pp;
