2fdb1d7d54275213578cbc356ca28038b17f4564
Sebabin <sebastiancrespin279@gmail.com>
2023-03-16 14:23:02 -0600

    fixed dropdown in header to use react state for opening and closing rather than tailwind classes hopefully this fixes ios issue

--- crushit-catering/components/NavMenu.js
+++ crushit-catering/components/NavMenu.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 /**imports to use with next auth*/
 import { useSession, signOut} from 'next-auth/react';
 import {CgMenuRound} from 'react-icons/cg'
 const NavMenu = () => {
+  const [open, setOpen] = useState(false);
     const { data: session } = useSession()
     const { push } = useRouter()
 	const handleSignOut = async () => {
@@ -14,7 +16,8 @@ const NavMenu = () => {
 
     return(
         <button
-      className="dropdown:block
+       onClick={() => setOpen(!open)}
+      className="
        relative
        px-2
        py-1
@@ -60,8 +63,8 @@ const NavMenu = () => {
         </svg>
       </div>
       <ul
-        className="left-0
-         hidden
+        className={`left-0
+          ${open ? 'h-30':'hidden'}
          w-auto
          p-1
          mt-6
@@ -73,30 +76,72 @@ const NavMenu = () => {
          rounded-lg
          shadow-lg
          xl:mt-8
-         "
+         `}
         aria-label="submenu"
       >
         <li
-          className="w-full p-1 font-medium text-site-yellow transition-colors duration-150 rounded-md hover:text-site-red focus:outline-none focus:shadow-outline hover:bg-gray-100"
-          
+          className="w-full
+           p-1
+           font-medium
+           text-site-yellow
+           transition-colors
+           duration-150
+           rounded-md
+           hover:text-site-red
+           focus:outline-none
+           focus:shadow-outline
+           hover:bg-gray-100"
+          onClick={() => {setOpen(false)}}
         >
           <Link href='/menu'>food</Link>
         </li>
         <li
-          className="w-full p-1 font-medium text-site-yellow transition-colors duration-150 rounded-md hover:text-site-red focus:outline-none focus:shadow-outline hover:bg-gray-100"
-         
+          className="w-full
+           p-1
+           font-medium
+           text-site-yellow
+           transition-colors
+           duration-150
+           rounded-md
+           hover:text-site-red
+           focus:outline-none
+           focus:shadow-outline
+           hover:bg-gray-100"
+         onClick={() => {setOpen(false)}}
         >
           <Link href='/'>home</Link>
         </li>
         {session ?
          <li
-         className="w-full p-1 font-medium text-site-yellow transition-colors duration-150 rounded-md hover:text-site-red focus:outline-none focus:shadow-outline hover:bg-gray-100"
+         className="w-full p-1
+          font-medium
+          text-site-yellow 
+          transition-colors
+          duration-150
+          rounded-md
+          hover:text-site-red
+         focus:outline-none 
+         focus:shadow-outline 
+         hover:bg-gray-100"
+         onClick={() => {setOpen(false)}}
        >
         <span onClick={handleSignOut}>logout</span>
         </li>
         :
         <li
-        className="w-full p-1 font-medium text-site-yellow transition-colors duration-150 rounded-md hover:text-site-red focus:outline-none focus:shadow-outline hover:bg-gray-100"
+        className="
+         w-full
+         p-1
+         font-medium
+         text-site-yellow
+         transition-colors
+         duration-150
+         rounded-md
+         hover:text-site-red
+         focus:outline-none
+         focus:shadow-outline
+         hover:bg-gray-100"
+         onClick={() => {setOpen(false)}}
       >
         <Link href='/signin'>login</Link>
         </li>
